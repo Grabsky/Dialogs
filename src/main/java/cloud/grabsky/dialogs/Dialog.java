a913bcfb704efd35e918bba2b940554597b9d0c8
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-08-26 10:29:21 +0200

    repeat tasks only when necessary, improve cancellation logic
    Tasks won't be cancelled immediately now but rather be invalidated and cancelled when they're executed, even if player has joined back. This solution is cleaner than building and accessing a task queue.

--- src/main/java/cloud/grabsky/dialogs/Dialog.java
+++ src/main/java/cloud/grabsky/dialogs/Dialog.java
@@ -31,7 +31,6 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,7 +38,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.function.BiPredicate;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -60,106 +58,72 @@ public final class Dialog implements Collection<DialogElement> {
         long nextTaskStartsIn = 0;
         // Iterating over all elements in this Dialog and scheduling to display them.
         for (final DialogElement element : elements) {
-            if (element instanceof AnimatedTextElement animatedText) {
-                // Scheduling and adding task to the tasks list.
-                queue.add(plugin.getBedrockScheduler().repeatAsync(
-                        nextTaskStartsIn,
-                        animatedText.refreshRate(),
-                        element.ticksToWait() / animatedText.refreshRate(),
-                        createPredicate(queue, target, element))
-                );
-                // ...
-                nextTaskStartsIn += element.ticksToWait() + animatedText.refreshRate(); // Two additional ticks had to be added, possibly due to a bug. Will be investigated later on.
-                // Continuing...
-                continue;
-            }
-            // Scheduling and adding task to the tasks list.
-            queue.add(plugin.getBedrockScheduler().repeatAsync(nextTaskStartsIn, 2L, element.ticksToWait() / 2, createPredicate(queue, target, element)));
-            // ...
-            nextTaskStartsIn += element.ticksToWait() + 2L; // Two additional ticks had to be added, possibly due to a bug. Will be investigated later on.
-        }
-    }
 
-    private @NotNull BiPredicate<BukkitRunnable, Integer> createPredicate(final List<BukkitTask> queue, final Player target, final DialogElement element) throws IllegalArgumentException {
-        if (element instanceof TextElement textElement) {
-            // Parsing the message, setting placeholders if supported.
-            final Message.StringMessage message = (Dialogs.isPlaceholderAPI() == true)
-                    ? Message.of(PlaceholderAPI.setPlaceholders(target, textElement.value()))
-                    : Message.of(textElement.value());
-            // Returning...
-            return (runnable, iteration) -> {
-                // Cancelling all remaining tasks when target happen to be offline.
-                if (target == null || target.isOnline() == false) {
-                    // Cancelling all OTHER tasks.
-                    queue.stream().filter(queuedTask -> queuedTask.getTaskId() != runnable.getTaskId()).forEach(BukkitTask::cancel);
-                    // Cancelling THIS task.
-                    return false;
-                }
-                // Getting the message channel.
-                final TextElement.Channel channel = textElement.channel();
-                // Sending message based on channel type.
-                switch (channel) {
-                    case CHAT_MESSAGE -> message.send(target);
-                    case CHAT_BROADCAST -> message.broadcast();
-                    case ACTIONBAR -> {
-                        message.sendActionBar(target);
-                        // Returning true when TextElement#lockUntilNextElement is true. Makes the task re-execute until iterations limit is reached.
-                        return textElement.lockUntilNextElement() == true;
+            if (element instanceof AnimatedTextElement animatedText) {
+                final AnimatedTextElement.Channel channel = animatedText.channel();
+                final Iterator<Component> frames = animatedText.frames().iterator();
+                // Scheduling a new asynchrnous repeat task.
+                plugin.getBedrockScheduler().repeatAsync(nextTaskStartsIn, animatedText.refreshRate(), element.ticksToWait() / animatedText.refreshRate(), (iteration) -> {
+                    // Cancelling task in case Player connection has been reset.
+                    if (target.isConnected() == false)
+                        return false;
+                    // Preparing the message Component.
+                    final Component component = (frames.hasNext() == true)
+                            ? frames.next()
+                                    : (animatedText.lockUntilNextElement() == true)
+                                    ? animatedText.lastFrame()
+                            : null;
+                    // Currently only action bar messages can be "animated".
+                    switch (channel) {
+                        case ACTIONBAR -> Message.of(component).sendActionBar(target);
                     }
-                }
-                // Exiting...
-                return false;
-            };
-        } else if (element instanceof AnimatedTextElement animatedText) {
-            final AnimatedTextElement.Channel channel = animatedText.channel();
-            final Iterator<Component> frames = animatedText.frames().iterator();
-            // Returning...
-            return (runnable, iteration) -> {
-                // Cancelling all remaining tasks when target happen to be offline.
-                if (target == null || target.isOnline() == false) {
-                    // Cancelling all OTHER tasks.
-                    queue.stream().filter(queuedTask -> queuedTask.getTaskId() != runnable.getTaskId()).forEach(BukkitTask::cancel);
-                    // Cancelling THIS task.
-                    return false;
-                }
-                // Preparing the message Component.
-                final Component component = (frames.hasNext() == true)
-                        ? frames.next()
-                        : (animatedText.lockUntilNextElement() == true)
-                                ? animatedText.lastFrame()
-                                : null;
-                // Currently only action bar messages can be "animated".
-                switch (channel) {
-                    case ACTIONBAR -> Message.of(component).sendActionBar(target);
-                }
-                if (frames.hasNext() == true)
-                    target.playSound(target, Sound.BLOCK_NOTE_BLOCK_HAT, 1.0F, 0.1F);
-                // Continuing... Should be cancelled automatically when max iterations is reached.
-                return true;
-            };
-        } else if (element instanceof ConsoleCommandElement consoleCommand) {
-            // Returning...
-            return (runnable, iteration) -> {
-                // Cancelling all remaining tasks when target happen to be offline.
-                if (target == null || target.isOnline() == false) {
-                    // Cancelling all OTHER tasks.
-                    queue.stream().filter(queuedTask -> queuedTask.getTaskId() != runnable.getTaskId()).forEach(BukkitTask::cancel);
-                    // Cancelling THIS task.
-                    return false;
-                }
-                // Scheduling command execution to the main thread.
-                plugin.getBedrockScheduler().run(1L, (___) -> {
+                    if (frames.hasNext() == true)
+                        target.playSound(target, Sound.BLOCK_NOTE_BLOCK_HAT, 1.0F, 0.1F);
+                    // Continuing... Should be cancelled automatically when max iterations is reached.
+                    return true;
+                });
+                // Calculating "start" time of the next element. Additionally, refresh rate value is added as to prevent elements from overlapping.
+                nextTaskStartsIn += element.ticksToWait() + animatedText.refreshRate();
+            }
+
+            else if (element instanceof TextElement textElement) {
+                // Parsing the message, setting placeholders if supported.
+                final Message.StringMessage message = (Dialogs.isPlaceholderAPI() == true)
+                        ? Message.of(PlaceholderAPI.setPlaceholders(target, textElement.value()))
+                        : Message.of(textElement.value());
+                // Scheduling a new asynchrnous run task.
+                plugin.getBedrockScheduler().runAsync(nextTaskStartsIn, (task) -> {
+                    // Skipping task execution in case Player connection has been reset.
+                    if (target.isConnected() == false)
+                        return;
+                    // Getting the message channel.
+                    final TextElement.Channel channel = textElement.channel();
+                    // Sending message based on channel type.
+                    switch (channel) {
+                        case CHAT_MESSAGE -> message.send(target);
+                        case CHAT_BROADCAST -> message.broadcast();
+                        case ACTIONBAR -> message.sendActionBar(target);
+                    }
+                });
+                // Calculating "start" time of the next element.
+                nextTaskStartsIn += element.ticksToWait();
+            }
+
+            else if (element instanceof ConsoleCommandElement consoleCommand) {
+                // Scheduling a new run task. Command dispatch have to be called on the main thread.
+                plugin.getBedrockScheduler().run(nextTaskStartsIn, (task) -> {
+                    // Skipping task execution in case Player connection has been reset.
+                    if (target.isConnected() == false)
+                        return;
                     // Preparing command string.
                     final String command = (Dialogs.isPlaceholderAPI() == true) ? PlaceholderAPI.setPlaceholders(target, consoleCommand.value()) : consoleCommand.value();
                     // Dispatching the command.
                     plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), command);
                 });
-                // Exiting...
-                return false;
-            };
+                // Calculating "start" time of the next element.
+                nextTaskStartsIn += element.ticksToWait();
+            }
         }
-        // ...
-        throw new IllegalArgumentException("Dialog element instance of class \"" + element.getClass().getSimpleName() + "\" is not supported.");
     }
 
 }

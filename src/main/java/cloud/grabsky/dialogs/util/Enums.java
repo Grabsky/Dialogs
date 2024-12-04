7ca019291ea00fd6e2241eb38a23ed97e2ec2b06
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-12-04 13:53:09 +0100

    relicense under GPLv3

--- src/main/java/cloud/grabsky/dialogs/util/Enums.java
+++ src/main/java/cloud/grabsky/dialogs/util/Enums.java
@@ -1,3 +1,17 @@
+/*
+ * Dialogs (https://github.com/Grabsky/Dialogs)
+ *
+ * Copyright (C) 2024  Grabsky <michal.czopek.foss@proton.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3 for more details.
+ */
 package cloud.grabsky.dialogs.util;
 
 import org.jetbrains.annotations.NotNull;
